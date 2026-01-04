b4925b461317f7799f747f7cc09122ff53af1b79
Wasit Limprasert <wasit7@gmail.com>
2026-01-04 23:32:46 +0700

    update version3

--- v3.13/line_clone_v3_13/app/chat_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index_view, name='index'), path('login/', views.auth_view, {'auth_type': 'login'}, name='login'),
-    path('signup/', views.auth_view, {'auth_type': 'signup'}, name='signup'), path('logout/', views.logout_view, name='logout'),
-    path('dm/<str:username>/', views.start_dm, name='start_dm'), path('chat/<str:room_name>/', views.room_view, name='chat_room'),
-    path('api/history/<str:room_name>/', views.history_api, name='history_api'),
-]
--- v3.13/line_clone_v3_13/app/line_project/asgi.py
+++
@@ -1,8 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'line_project.settings')
-django.setup()
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chat_app.routing import websocket_urlpatterns
-application = ProtocolTypeRouter({"http": get_asgi_application(), "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))})
